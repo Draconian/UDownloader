edb31f662391b61a5e391b7de4aab71b4c9f2bee
Draconian <dufault.f@gmail.com>
2013-01-05 00:19:09 -0500

    Apply style to DownloadVideo class

--- UDL/Model/DownloadVideo.cs
+++ UDL/Model/DownloadVideo.cs
@@ -15,26 +15,26 @@ namespace UDL.Model
         private static readonly int HISTORIC_DOWNLOAD_MAX = 5;
         private static readonly int TIMER_DOWNLOAD_INTERVAL = 1000;
 
-        private ulong _downloadedSize = 0;
-        private ulong _previousDownloadSize = 0;
+        private ulong downloadedSize = 0;
+        private ulong previousDownloadSize = 0;
 
-        private List<int> _historicDownloadSpeedPerSecond = new List<int>();
-        private VideoURL _videoURLDownload = null;
-        private String _outputFolder = null;
-        private Timer _timerDownload = null;
+        private List<int> historicDownloadSpeedPerSecond = new List<int>();
+        private VideoURL videoURLDownload = null;
+        private String outputFolder = null;
+        private Timer timerDownload = null;
         
-        private bool _isDownloading = false;
-        private bool _isDownloadFinish = false;
+        private bool isDownloading = false;
+        private bool isDownloadFinish = false;
 
         public DownloadVideo(VideoURL aVideoURL, String aOutputFolder)
         {
-            this._videoURLDownload = aVideoURL;
-            this._outputFolder = aOutputFolder;
+            this.videoURLDownload = aVideoURL;
+            this.outputFolder = aOutputFolder;
 
-            this._timerDownload = new Timer();
+            this.timerDownload = new Timer();
 
-            this._timerDownload.Elapsed += new ElapsedEventHandler(timerSecond_Tick);
-            this._timerDownload.Interval = TIMER_DOWNLOAD_INTERVAL;
+            this.timerDownload.Elapsed += new ElapsedEventHandler(timerSecond_Tick);
+            this.timerDownload.Interval = TIMER_DOWNLOAD_INTERVAL;
         }
 
 
@@ -43,41 +43,41 @@ namespace UDL.Model
         {
             get
             {
-                return Path.Combine(this._outputFolder, String.Format("{0}_{1}.{2}", this._videoURLDownload.Video.Author, 
-                    this._videoURLDownload.Video.Title, 
-                    this._videoURLDownload.FileExtension));
+                return Path.Combine(this.outputFolder, String.Format("{0}_{1}.{2}", this.videoURLDownload.Video.Author, 
+                    this.videoURLDownload.Video.Title, 
+                    this.videoURLDownload.FileExtension));
             }
         }
 
         public float Lenght
         {
-            get { return (float)this._videoURLDownload.Size; }
+            get { return (float)this.videoURLDownload.Size; }
         }
 
         public float DownloadedLenght
         {
-            get { return (float)this._downloadedSize; }
+            get { return (float)this.downloadedSize; }
         }
 
         public bool IsDownloading
         {
-            get { return this._isDownloading && !this._isDownloadFinish; }
+            get { return this.isDownloading && !this.isDownloadFinish; }
         }
         #endregion
 
         public void Download()
         {
-            if (this._isDownloading)
+            if (this.isDownloading)
             {
                 throw new Exception("Video already downloading");
             }
-            this._timerDownload.Start();
+            this.timerDownload.Start();
 
             byte[] buffer = new byte[10000];
 
             Debug.WriteLine("Download started");
 
-            HttpWebRequest webRequest = WebRequest.Create(this._videoURLDownload.DownloadURL) as HttpWebRequest;
+            HttpWebRequest webRequest = WebRequest.Create(this.videoURLDownload.DownloadURL) as HttpWebRequest;
             HttpWebResponse webResponse = webRequest.GetResponse() as HttpWebResponse;
 
             BinaryWriter binWriter = new BinaryWriter(File.Open(this.LocalPath, FileMode.Create));
@@ -88,7 +88,7 @@ namespace UDL.Model
                 int nbByteReads = binReader.Read(buffer, 0, buffer.Length);
                 if (nbByteReads == 0) break;
 
-                this._downloadedSize += (ulong)nbByteReads;
+                this.downloadedSize += (ulong)nbByteReads;
                 binWriter.Write(buffer, 0, nbByteReads);
 
             }
@@ -99,55 +99,18 @@ namespace UDL.Model
 
             Debug.WriteLine("Download done");
 
-            this._isDownloadFinish = true;
-            this._timerDownload.Stop();
+            this.isDownloadFinish = true;
+            this.timerDownload.Stop();
         }
 
         private void timerSecond_Tick(object sender, System.Timers.ElapsedEventArgs e)
         {
-            Debug.WriteLine(String.Format("{0} | {1} \\/ {2}",this._downloadedSize,this._previousDownloadSize,(int)(this._downloadedSize - this._previousDownloadSize)));
-            this._historicDownloadSpeedPerSecond.Add((int)(this._downloadedSize - this._previousDownloadSize));
-            this._previousDownloadSize = this._downloadedSize;
+            Debug.WriteLine(String.Format("{0} | {1} \\/ {2}",this.downloadedSize,this.previousDownloadSize,(int)(this.downloadedSize - this.previousDownloadSize)));
+            this.historicDownloadSpeedPerSecond.Add((int)(this.downloadedSize - this.previousDownloadSize));
+            this.previousDownloadSize = this.downloadedSize;
 
             this.NotifyObservers();
         }
-        /*
-        private void DownloadProgress()
-        {
-             
-
-            float percentage = 0;
-            ulong previousSize = 0;
-            int historicIndex = 0;
-            
-            while (true)
-            {
-                Thread.Sleep(1000);
-                int bytePerSecond = (int)(this._downloadedSize - previousSize);
-
-                historicBytePerSecond[historicIndex] = bytePerSecond;
-
-                String update = String.Format("{0}/sec | {1} on {2} downloaded. {3:0.00}%", ConvertByteString(AverageBytePerSecond(historicBytePerSecond)), ConvertByteString(this._downloadedSize), ConvertByteString(this._totalSize), percentage);
-
-                if (this._totalSize > 0)
-                {
-                    percentage = (float)((this._downloadedSize * 100.0) / this._totalSize);
-                }
-
-                base.NotifyObservers();
-
-                //this._backGroundWorker.ReportProgress((int)percentage, update);
-
-                Debug.WriteLine(update);
-                historicIndex++;
-
-                previousSize = this._downloadedSize;
-
-                if (historicIndex >= HISTORIC_DOWNLOAD_MAX)
-                {
-                    historicIndex = 0;
-                }
-            }*/
 
         public float AverageDownloadSpeed()
         {
@@ -155,11 +118,11 @@ namespace UDL.Model
             float averageDownloadSpeed = 0.0f;
 
             
-            this._historicDownloadSpeedPerSecond.Select(speed => { speedTotal += speed; return speed; }).ToList();
+            this.historicDownloadSpeedPerSecond.Select(speed => { speedTotal += speed; return speed; }).ToList();
 
-            if (_historicDownloadSpeedPerSecond.Count > 0)
+            if (historicDownloadSpeedPerSecond.Count > 0)
             {
-                averageDownloadSpeed = (float)speedTotal / (float)this._historicDownloadSpeedPerSecond.Count;
+                averageDownloadSpeed = (float)speedTotal / (float)this.historicDownloadSpeedPerSecond.Count;
             }
                 
             return averageDownloadSpeed;
